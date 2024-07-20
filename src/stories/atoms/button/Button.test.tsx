a88f8f10b7e08b930a5a72d545d95d9c0b6592cf
antonionicolas___ <antonionicolas3003@gmail.com>
2024-07-20 11:25:38 -0300

    feat: adding tests config and button test

---
+++ src/stories/atoms/button/Button.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { Button } from './Button'
+import type { Props } from './Button.types'
+describe('Button', () => {
+  let defaultProps: Props
+
+  beforeEach(() => {
+    defaultProps = {
+      label: 'Hello world',
+      onClick: vi.fn()
+    }
+  })
+  it('Should render base Button', () => {
+    render(<Button {...defaultProps} />)
+    const btn = screen.getByRole('button', { name: 'Hello world' })
+    expect(btn).toBeDefined()
+    expect(btn.getAttribute('variant')).toBe('primary')
+  })
+  it('Should render the base button with secondary variant', () => {
+    defaultProps.variant = 'secondary'
+    render(<Button {...defaultProps} />)
+
+    const btn = screen.getByRole('button', { name: 'Hello world' })
+    expect(btn).toBeDefined()
+    expect(btn.getAttribute('variant')).toBe('secondary')
+  })
+  it('should call the onClick callback', () => {
+    render(<Button {...defaultProps} />)
+    const btn = screen.getByRole('button', { name: 'Hello world' })
+    fireEvent.click(btn)
+    expect(defaultProps.onClick).toHaveBeenCalledOnce()
+  })
+})
