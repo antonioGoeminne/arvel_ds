ad5ebde7f4d6a35cc03d4cef1198dbc4acd909b8
antonionicolas___ <antonionicolas3003@gmail.com>
2024-07-21 12:46:49 -0300

    feat: tag test

--- src/stories/atoms/tag/Tag.tsx
+++ src/stories/atoms/tag/Tag.tsx
@@ -2,5 +2,12 @@ import * as Styled from './Tag.styled'
 import { Props } from './Tag.types'
 
 export const Tag: React.FC<Props> = ({ variant = 'default', label }) => {
-  return <Styled.Tag variant={variant}>{label}</Styled.Tag>
+  return (
+    <Styled.Tag
+      data-testid="tag"
+      variant={variant}
+    >
+      {label}
+    </Styled.Tag>
+  )
 }
