f824685bce330a22ce5c9d2aaee80b21999961b0
antonionicolas___ <antonionicolas3003@gmail.com>
2024-07-22 09:33:04 -0300

    feat: adding avatar test

---
+++ src/stories/atoms/avatar/Avatar.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import { Avatar } from './Avatar'
+import { Props } from './Avatar.types'
+
+describe('Avatar', () => {
+  let defaultProps: Props
+
+  beforeEach(() => {
+    defaultProps = {
+      src: 'some-src',
+      alt: 'some-alt'
+    }
+  })
+  it('Should render avatar', () => {
+    render(<Avatar {...defaultProps} />)
+    const element = screen.getByAltText('some-alt')
+    expect(element).toBeDefined()
+    expect(element.getAttribute('src')).toBe('some-src')
+  })
+})
