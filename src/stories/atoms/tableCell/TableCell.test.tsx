3b120dffb608eb5684c58a93eaca35d3db84ef04
antonionicolas___ <antonionicolas3003@gmail.com>
2024-07-23 10:33:22 -0300

    feat: adding tableCell tests

--- src/stories/atoms/tableCell/TableCell.test.tsx
+++ src/stories/atoms/tableCell/TableCell.test.tsx
@@ -17,10 +17,38 @@ describe('TableCell', () => {
     expect(cell.getAttribute('variant')).toBe('default')
   })
 
-  it('Should render base tableCell', () => {
+  it('Should render tag cell', () => {
+    defaultProps.variant = 'tag'
+    defaultProps.label = 'tag'
+    render(<TableCell {...defaultProps} />)
+    const cell = screen.getByTestId('tagCell')
+    expect(cell).toBeDefined()
+    expect(cell.getAttribute('variant')).toBe('tag')
+
+    const tag = screen.getByTestId('tag')
+    expect(tag).toBeDefined()
+  })
+
+  it('Should render avatar cell', () => {
+    defaultProps.variant = 'avatar'
+    defaultProps.src = 'some-src'
+    defaultProps.alt = 'some-alt'
+
+    render(<TableCell {...defaultProps} />)
+    const cell = screen.getByTestId('avatarCell')
+    expect(cell).toBeDefined()
+    expect(cell.getAttribute('variant')).toBe('avatar')
+
+    const avt = screen.getByTestId('avatar')
+    expect(avt).toBeDefined()
+  })
+
+  it('Should render price cell', () => {
+    defaultProps.variant = 'price'
+
     render(<TableCell {...defaultProps} />)
     const cell = screen.getByRole('cell')
     expect(cell).toBeDefined()
-    expect(cell.getAttribute('variant')).toBe('default')
+    expect(cell.getAttribute('variant')).toBe('price')
   })
 })
